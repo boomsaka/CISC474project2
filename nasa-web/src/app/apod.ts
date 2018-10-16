7e15fb8a29cbf4b7c4810e1551e302fe3e3ea732
yihanye <32300316+yihanye@users.noreply.github.com>
2018-10-16 14:01:46 -0400

    small example nasa-web

---
+++ nasa-web/src/app/apod.ts
@@ -0,0 +1,7 @@
+import { Url } from "url";
+
+export class Apod {
+    title: string;
+    date: string;
+    explanation: string;
+}
---
+++ nasa-web/src/app/apods/apods.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ApodsComponent } from './apods.component';
+
+describe('ApodsComponent', () => {
+  let component: ApodsComponent;
+  let fixture: ComponentFixture<ApodsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ApodsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ApodsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
